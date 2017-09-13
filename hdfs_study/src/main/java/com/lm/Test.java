b9a891b36c2a7f3de56f3f2254da8fcc3b9b1ed9
lm <hzlimao@corp.netease.com>
2017-09-13 10:44:21 +0800

    no message

---
+++ hdfs_study/src/main/java/com/lm/Test.java
@@ -0,0 +1,12 @@
+package com.lm;
+
+/**
+ * @author
+ * @create 2017-09-13 上午10:40
+ **/
+public class Test {
+
+        public static void main(String[] args) {
+                System.out.println(11);
+            }
+}

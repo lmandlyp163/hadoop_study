eda9d84f78c88e66901b375f716d34de5ab4480c
lm <hzlimao@corp.netease.com>
2017-10-26 15:24:32 +0800

    no message

---
+++ hdfs_study/src/main/java/com/lm/MR/JobTest_1.java
@@ -0,0 +1,56 @@
+package com.lm.MR;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * @author
+ * @create 2017-10-26 上午10:52
+ * 提交到测试环境：hadoop jar  ./hdfs.study-1.0-SNAPSHOT.jar  com.lm.MR.JobTest_1
+ * 注意设置文件权限
+ * -DHADOOP_USER_NAME=hadoop
+ **/
+public class JobTest_1 {
+
+    public static void main(String[] args) throws Exception {
+        //输入路径
+        String dst = "hdfs://172.16.241.134:9000/test.txt";
+        //输出路径，必须是不存在的，空文件加也不行。
+        String dstOut = "hdfs://172.16.241.134:9000/output/test2";
+        Configuration hadoopConfig = new Configuration();
+
+        hadoopConfig.set("fs.hdfs.impl",
+                org.apache.hadoop.hdfs.DistributedFileSystem.class.getName()
+        );
+        hadoopConfig.set("fs.file.impl",
+                org.apache.hadoop.fs.LocalFileSystem.class.getName()
+        );
+        Job job = Job.getInstance(hadoopConfig,"hadoop_job_name");
+        job.setUser("hadoop");
+
+        //如果需要打成jar运行，需要下面这句
+        job.setJarByClass(JobTest_1.class);
+
+        //job执行作业时输入和输出文件的路径
+        FileInputFormat.setInputPaths(job, new Path(dst));
+        FileOutputFormat.setOutputPath(job, new Path(dstOut));
+
+
+        //指定自定义的Mapper和Reducer作为两个阶段的任务处理类
+        job.setMapperClass(MapperTest_1.class);
+        job.setReducerClass(ReducerTest_1.class);
+
+        //设置最后输出结果的Key和Value的类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //执行job，直到完成
+        job.waitForCompletion(true);
+        System.out.println("Finished");
+    }
+}
---
+++ hdfs_study/src/main/java/com/lm/MR/MapperTest_1.java
@@ -0,0 +1,41 @@
+package com.lm.MR;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @author
+ * @create 2017-10-26 上午10:52
+ **/
+public class MapperTest_1 extends
+        Mapper<LongWritable, Text, Text, IntWritable> {
+
+    /**
+     * 四个泛型类型分别代表：
+     * KeyIn        Mapper的输入数据的Key，这里是每行文字的起始位置（0,11,...）
+     * ValueIn      Mapper的输入数据的Value，这里是每行文字
+     * KeyOut       Mapper的输出数据的Key，这里是每行文字中的“年份”
+     * ValueOut     Mapper的输出数据的Value，这里是每行文字中的“气温”
+     * 2014010114
+     * 2014010216
+     */
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        // 打印样本: Before Mapper: 0, 2000010115
+        System.out.print("Before Mapper: " + key + ", " + value);
+        String line = value.toString();
+        String year = line.substring(0, 4);
+        int temperature = Integer.parseInt(line.substring(8));
+        context.write(new Text(year), new IntWritable(temperature));
+        // 打印样本: After Mapper:2000, 15
+        System.out.println(
+                "======" +
+                        "After Mapper:" + new Text(year) + ", " + new IntWritable(temperature));
+    }
+
+}
